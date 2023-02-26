deb6d29a3bcd886c8bef0e412f2ff7f341771762
KadenWingert1 <swiperkcw@gmail.com>
2023-02-26 15:19:24 -0600

    added js code to put intom JSON...Doesn't work yet

--- Nordland Forge/index.js
+++ Nordland Forge/index.js
@@ -10,3 +10,25 @@ function openNavBar() {
       logo.style.display = "block";
     }
   }
+
+  /*Used to put user entered info into JSON*/
+  const formEl = document.querySelector(".form");
+  if(formEl){
+    console.log("It is working");
+  formEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(formEl);
+    const data = Object.fromEntries(formData);
+    fetch('https://reqres/in/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('error '))
+  }); 
+}
+/*End of code for convertnig to JSON*/
